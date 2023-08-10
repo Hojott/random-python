b42d0d56165905db7a31ad515788ace76d5873e0
Sakari Marttinen <sakkenino@gmail.com>
2023-08-10 20:40:12 +0300

    Remove hanging files from root

--- setelit.py
+++
@@ -1,6 +0,0 @@
-result = []
-for twenty in range (20): # 370/20 = n. 20
-    for fifty in range (8):
-        if twenty*20 + fifty*50 == 370:
-            result.append((twenty, fifty))
-print(result)
